642431030d76dfd05c0043a14ffdf7ea19724ddc
Ulises Cervantes Torres <ulises.cerv@hotmail.comcom>
2020-07-30 14:47:53 -0500

    about me

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import  "./About.css";
+
+export default function About() {
+    return (
+        <div className="about">
+            <div className="info">
+                <div className="title">
+                    <span>Acerca de mi</span>
+                    <p className="phrase">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore repellat sed libero incidunt voluptas iusto, eos nulla a magnam eum culpa aliquid, aspernatur dignissimos, perspiciatis accusantium corrupti ipsam quos neque numquam. Harum quis fuga nulla ad omnis beatae deserunt vero.</p>
+                    <div className="followMeBtn">Follow me!</div>
+                </div>
+            </div>
+            <div className="beto_img_container">
+                <img src={require(`../../Assets/images/beto-about_450.png`)}  alt="Beto" id="beto_img"/>
+            </div>
+        </div>
+    )
+}
--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -3,12 +3,14 @@ import './MainView.css';
 
 import Header from  '../Header/Header'
 import Menu from '../Menu/Menu'
+import About from '../About/About'
 
 const MainView = () => {
     return (
         <div className="main">
             <Menu/>
             <Header/>
+            <About/>
         </div>
     );
 }
